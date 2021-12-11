8ab328414570fc89d28dec57c3c98dce50a12b4e
chenyuqi-12138 <1097620517@qq.com>
2021-12-11 16:09:24 +0800

    test

--- src/main/java/com/tools/git/testcommit/MonitorApplication.java
+++ src/main/java/com/tools/git/testcommit/MonitorApplication.java
@@ -1,7 +1,13 @@
 package com.tools.git.testcommit;
 
+import com.tools.git.testcommit.model.User;
+
 public class MonitorApplication {
 	public static void main(String[] args) {
 		System.out.println("asdasdasdas");
+		User user = new User();
+		
+		user.setAge(12);
+		System.out.println(user);
 	}
 }
